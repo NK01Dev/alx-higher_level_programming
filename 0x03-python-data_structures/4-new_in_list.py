f6ee673ce70ddf6aa37f384e2198c0b758d747a3
NK01Dev <kamal.nk.naim@gmail.com>
2023-11-07 11:16:57 -0800

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+    if idx < 0 or len(my_list) - 1 < idx:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
+
